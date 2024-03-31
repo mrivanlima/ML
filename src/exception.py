1866e212446d4bae85c5d4fd79e13fe9a26ca9fd
mrivanlima <mrivanlima@gmail.com>
2024-03-31 00:22:45 -0400

    Testing structure

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,9 @@
 import sys
 
+from src.logger import logging
+
 def error_message_details(error, error_detail:sys):
-    _,_,exc_tb = error_detail.exc_info
+    _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_line = exc_tb.tb_lineno
     error_message = "Error occurred in python script name [{0}] line number [{1}] error message[{2}]".format(
@@ -20,3 +22,11 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+
+ #Test it writes in the log file   
+# if __name__=="__main__":
+#     try:
+#         a = 1 / 0
+#     except Exception as e:
+#         logging.info("Divide by zero")
+#         raise CustomException(e, sys)
